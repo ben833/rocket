fadf63921d8d6952bea8cff9ac021fa29270a12f
Ben Margolis <ben833@gmail.com>
2023-10-06 13:13:57 -0400

    Code cleanup and close response body

--- main.go
+++ main.go
@@ -26,12 +26,15 @@ func main() {
 	// Command line argument for how many dollars the user wants to invest
 	if len(os.Args) > 1 {
 		var err error
-		investmentAmount, err = strconv.ParseFloat(os.Args[1], 32)
+		investmentAmount, err = strconv.ParseFloat(os.Args[1], 64)
 		if err != nil {
 			fmt.Println("Error parsing the amount to invest as a float: ", err)
 			return
 		}
-
+		if investmentAmount <= 0 {
+			fmt.Println("Please provide a positive investment amount")
+			return
+		}
 	} else {
 		fmt.Println("Missing argument for amount to invest.")
 		fmt.Println("Usage: go run main.go [amount]")
@@ -39,55 +42,56 @@ func main() {
 		return
 	}
 
-	// get data from Coinbase's API
+	// Get data from Coinbase's API
 	response, err := http.Get(url)
 	if err != nil {
-		fmt.Println("error occurred trying to get data from the API: ", err)
+		fmt.Println("Error occurred trying to get data from the API: ", err)
 		return
 	}
+	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		fmt.Println("error happened when reading the response body: ", err)
+		fmt.Println("Error happened when reading the response body: ", err)
 		return
 	}
 
 	// I wanted to use the variable name "err" here, but I got a compiler error
 	// "no new variables on left side of :=" (NoNewVar)
 	// It's weird because I used that pattern twice above
-	error := json.Unmarshal(body, &coinbaseResult)
-	if error != nil {
-		fmt.Println("error from doing json.Unmarshal: ", error)
+	jsonError := json.Unmarshal(body, &coinbaseResult)
+	if jsonError != nil {
+		fmt.Println("Error from doing json.Unmarshal: ", jsonError)
 		return
 	}
 
-	// price of BTC
+	// Price of BTC
 	btcRate, _ := GetRate(coinbaseResult, "BTC")
 	if btcRate <= 0 {
 		fmt.Println("Invalid rate for BTC")
 		return
 	}
 
-	// price of ETH
+	// Price of ETH
 	ethRate, _ := GetRate(coinbaseResult, "ETH")
 	if ethRate <= 0 {
 		fmt.Println("Invalid rate for ETH")
 		return
 	}
 
-	// how much BTC i get with 70% of dollars
+	// How much BTC I get with 70% of investment dollars
 	btcInvestment := 0.7 * investmentAmount * btcRate
 
-	// how much ETH i get with 30% of dollars
+	// How much ETH I get with 30% of investment dollars
 	ethInvestment := 0.3 * investmentAmount * ethRate
 
-	// output JSON with eth: eth_amount, btc: btc_amount
+	// Output JSON with eth: eth_amount, btc: btc_amount
 	result, err := json.Marshal(map[string]float64{
 		"btc": btcInvestment,
 		"eth": ethInvestment,
 	})
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Println("Error trying to marshal: ", err)
 		return
 	}
 	fmt.Println(string(result))
@@ -95,7 +99,7 @@ func main() {
 
 func GetRate(coinbaseResult CoinbaseResult, crypto string) (float64, error) {
 	rate := coinbaseResult.Data.Rates[crypto]
-	amount, err := strconv.ParseFloat(rate, 32)
+	amount, err := strconv.ParseFloat(rate, 64)
 	if err != nil {
 		fmt.Println("Error parsing the rate as a float: ", err)
 		return 0, err
